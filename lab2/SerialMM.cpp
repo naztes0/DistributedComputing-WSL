07e2b5b313c7703f2ac25ec76ad2f76c5a54ca24
naztes0 <naztes0@gmail.com>
2025-10-15 13:32:18 +0000

    Added func of random init instead of dummy init and added calculation execution timer

--- lab2/SerialMM.cpp
+++ lab2/SerialMM.cpp
@@ -13,6 +13,19 @@ void DummyDataInitialization(double *pAMatrix, double *pBMatrix, int Size)
         }
     }
 }
+// Function for random initialization of matrix elements
+void RandomDataInitialization(double *pAMatrix, double *pBMatrix,
+                              int Size)
+{
+    int i, j; // Loop variables
+    srand(unsigned(clock()));
+    for (i = 0; i < Size; i++)
+        for (j = 0; j < Size; j++)
+        {
+            pAMatrix[i * Size + j] = rand() / double(1000000);
+            pBMatrix[i * Size + j] = rand() / double(1000000);
+        }
+}
 // Function for formatted matrix output
 void PrintMatrix(double *pMatrix, int RowCount, int ColCount)
 {
@@ -40,13 +53,27 @@ void ProcessInitialization(double *&pAMatrix, double *&pBMatrix, double *&pCMatr
     pBMatrix = new double[Size * Size];
     pCMatrix = new double[Size * Size];
     // Initialization of matrix elements
-    DummyDataInitialization(pAMatrix, pBMatrix, Size);
+    RandomDataInitialization(pAMatrix, pBMatrix, Size);
     for (int i = 0; i < Size * Size; i++)
     {
         pCMatrix[i] = 0;
     }
 }
+// Function for matrix multiplication
+void SerialResultCalculation(double *pAMatrix, double *pBMatrix, double *pCMatrix, int Size)
+{
 
+    for (int i = 0; i < Size; i++)
+    {
+        for (int j = 0; j < Size; j++)
+        {
+            for (int k = 0; k < Size; k++)
+            {
+                pCMatrix[i * Size + j] += pAMatrix[i * Size + k] * pBMatrix[k * Size + j];
+            }
+        }
+    }
+}
 // Function for computational process termination
 void ProcessTermination(double *pAMatrix, double *pBMatrix, double *pCMatrix)
 {
@@ -54,23 +81,35 @@ void ProcessTermination(double *pAMatrix, double *pBMatrix, double *pCMatrix)
     delete[] pBMatrix;
     delete[] pCMatrix;
 }
+
 int main()
 {
     double *pAMatrix; // First argument of matrix multiplication
     double *pBMatrix; // Second argument of matrix multiplication
     double *pCMatrix; // Result matrix
     int Size;         // Size of matrices
+    double start, finish, duration;
 
     printf("Serial matrix multiplication program\n");
 
     ProcessInitialization(pAMatrix, pBMatrix, pCMatrix, Size);
-
     // Matrix output
     printf("\nInitial A Matrix \n");
     PrintMatrix(pAMatrix, Size, Size);
     printf("\nInitial B Matrix \n");
     PrintMatrix(pBMatrix, Size, Size);
 
+    start = clock();
+    // Matrix multiplication
+    SerialResultCalculation(pAMatrix, pBMatrix, pCMatrix, Size);
+    // Printing the result matrix
+    finish = clock();
+    duration = (finish - start) / double(CLOCKS_PER_SEC);
+
+    printf("\n Result Matrix: \n");
+    PrintMatrix(pCMatrix, Size, Size);
+    // Printing the time spent by matrix multiplication
+    printf("\n Time of execution: %f\n", duration);
     // Computational process termination
     ProcessTermination(pAMatrix, pBMatrix, pCMatrix);
 }
